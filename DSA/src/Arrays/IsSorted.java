8e85d38d5f83c039689e3bda7f845b062ffd65b9
Faizan Pathan <pathanfaizan0202@gmail.com>
2026-02-21 15:21:07 +0530

    Update

--- DSA/src/Arrays/IsSorted.java
+++ DSA/src/Arrays/IsSorted.java
@@ -17,6 +17,7 @@ public class IsSorted {
 
     static boolean isSorted(int arr[] , int n){
         for (int i = 0; i < n-1; i++){
+
             if (arr[i] > arr[i+1]) {
                 return false;
             }
@@ -24,10 +25,26 @@ public class IsSorted {
         return true;
     }
 
+//    public static void sortArray(int arr[] , int n){
+//        for (int i=0; i<n-1; i++){
+//            if (arr[i] >= arr[i+1]){
+//                int temp = arr[i];
+//                arr[i] = arr[i+1];
+//                arr[i+1] = temp;
+//            }
+//        }
+//        for (int i=0; i<n; i++){
+//            System.out.println(arr[i] + " ");
+//        }
+//
+//
+//    }
+
 
     public static void main(String[] args) {
         int arr[] = {4,2,3,4,6,7};
         int n = arr.length;
+//        sortArray(arr,n);
         System.out.println(isSorted(arr,n));
     }
 }
--- DSA/src/Recursion/factorial.java
+++ DSA/src/Recursion/factorial.java
@@ -1,17 +1,23 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class factorial {
 
     public static int factorial(int n){
         if (n == 0 || n == 1) {
             return 1;
+
         }
         return n * factorial(n - 1);
+
     }
 
     public static void main(String[] args) {
-        int num = 5;
-        System.out.println("The factorial of " + num + " is " + factorial(num));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to find factorial: ");
+        int n = sc.nextInt();
+        System.out.println("The factorial of " + n + " is " + factorial(n));
     }
 }
 
